50dc95392a4b2f58e4e2aba435abc0bdcc988ee0
Josh <josh@maffey.com>
2021-02-05 10:18:33 +1300

    Update update_shickchill.sh
    Make into functions so they can be swapped out for different installation methods

--- update_shickchill.sh
+++ update_shickchill.sh
@@ -8,6 +8,39 @@ SICKCHILL_PID="$HOME/sickchill/SickChill.pid"
 
 ###################################################################################################################
 
+function check_pip() {
+    NV_DATA=$(${PIP_PATH}pip list --outdated | grep sickchill)
+    if [[ -n "$NV_DATA" ]]; then
+        NV_ARR=( $NV_DATA )
+        NV_REQUIRED=true
+        NV_O_VER=${NV_ARR[1]}
+        NV_N_VER=${NV_ARR[2]}
+    else
+        NV_DATA=$(${PIP_PATH}/pip freeze | grep sickchill)
+        NV_DATA=="${NV_DATA//==/$' '}"
+        NV_ARR=( $NV_DATA )
+        NV_O_VER=${NV_ARR[1]}
+        NV_N_VER=${NV_ARR[1]}
+    fi
+}
+
+function update_pip() {
+    ${PIP_PATH}pip -q install --upgrade sickchill
+}
+
+function update_git() {
+    git pull
+}
+
+function restart_sickchill() {
+    echo "Restarting Sickchill..."
+    pkill -9 -fu "$(whoami)" 'SickChill.py'
+    rm -rf ${SICKCHILL_PID}
+    ${PIP_PATH}/python ${PIP_PATH}/SickChill.py -d --pidfile="$SICKCHILL_PID"
+}
+
+###################################################################################################################
+
 echo '  _____   _          _             _       _   _   _     _    _               _           _                 '
 echo ' / ____| (_)        | |           | |     (_) | | | |   | |  | |             | |         | |                '
 echo '| (___    _    ___  | | __   ___  | |__    _  | | | |   | |  | |  _ __     __| |   __ _  | |_    ___   _ __ '
@@ -20,21 +53,12 @@ echo '**************************************************************************
 echo ''
 echo "Checking if an update is available..."
 
-# Check if there is an update available
-NV_DATA=$(${PIP_PATH}pip list --outdated | grep sickchill)
 NV_REQUIRED=false
-if [[ -n "$NV_DATA" ]]; then
-    NV_ARR=( $NV_DATA )
-    NV_REQUIRED=true
-    NV_O_VER=${NV_ARR[1]}
-    NV_N_VER=${NV_ARR[2]}
-else
-    NV_DATA=$(${PIP_PATH}/pip freeze | grep sickchill)
-    NV_DATA=="${NV_DATA//==/$' '}"
-    NV_ARR=( $NV_DATA )
-    NV_O_VER=${NV_ARR[1]}
-    NV_N_VER=${NV_ARR[1]}
-fi
+NV_O_VER=0
+NV_N_VER=0
+
+# Check if there is an update available
+check_pip
 
 echo ""
 echo "Installed version:  $NV_O_VER"
@@ -46,13 +70,11 @@ if [ "$NV_REQUIRED" = true ]; then
     echo ""
     # Perform the update
     echo "Updating Sickchill..."
-    ${PIP_PATH}pip -q install --upgrade sickchill
+    update_pip
+    
     echo ""
     # Restart Sickchill
-    echo "Restarting Sickchill..."
-    pkill -9 -fu "$(whoami)" 'SickChill.py'
-    rm -rf ${SICKCHILL_PID}
-    ${PIP_PATH}/python ${PIP_PATH}/SickChill.py -d --pidfile="$SICKCHILL_PID"
+    restart_sickchill
 else 
     echo "Sickchill is already the latest version!"
 fi
